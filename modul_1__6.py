9665163d3f74e92925121addce62f559096e581c
VladyTol1995 <tolstosheevvladimir4@gmail.com>
2024-11-29 21:20:12 +0300

    Add files via upload

---
+++ modul_1__6.py
@@ -0,0 +1,8 @@
+
+my_string = input("Enter your name and last name:")
+print ("Hello,", my_string)
+print ("Hello,", my_string.upper())
+print ("Hello,", my_string.lower())
+print ("Hello,", my_string.replace(' ',''))
+print ("Hello,", my_string [0])
+print ("Hello,", my_string [-1])
